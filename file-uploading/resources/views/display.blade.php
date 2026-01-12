973d54b8fcefa6ba0628adb7d6cff31f71d26a81
Sheth Uday <shethuday505@gmail.com>
2026-01-12 15:25:27 +0530

    use diffrent funciton to display the images

--- file-uploading/resources/views/display.blade.php
+++ file-uploading/resources/views/display.blade.php
@@ -1,5 +1,29 @@
-<div>
-    {{-- <img src="url('storage/app/private/avatars/.'{{ $file }})" alt="" srcset=""> --}}
-    <img src="{{ Storage::url( $file) }}" alt="user_image">
-    {{ Storage::url($file) }}
-</div>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>image display</title>
+</head>
+
+<body>
+    <div>
+
+        {{-- working --}}
+
+        <img src="{{ asset('storage/' . $file) }}" style="width: auto; height: 100px;">
+
+        {{-- non working --}}
+
+        {{-- <img src="{{ Storage::disk('public')->url($file) }}" alt="user_image"> --}}
+
+        {{-- <img src="{{ Storage::get('$file') }}" alt="user_image"> --}}
+
+        {{-- <img src="{{ Storage::url('$file') }}" alt="user_image"> --}}
+
+    </div>
+</body>
+
+</html>
