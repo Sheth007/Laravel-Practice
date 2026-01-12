9d1787f239c010bd9710a78d2ca09f5ae23205f4
Sheth Uday <shethuday505@gmail.com>
2026-01-12 17:32:35 +0530

    small changes

--- file-uploading/config/filesystems.php
+++ file-uploading/config/filesystems.php
@@ -42,7 +42,6 @@ return [
             'driver' => 'local',
             'root' => storage_path('app/public'),
             'url' => rtrim(env('APP_URL'), '/') . '/storage',
-            // 'url' => rtrim(env('http://localhost'), '/') . '/storage',
             'visibility' => 'public',
             'throw' => false,
             'report' => false,
